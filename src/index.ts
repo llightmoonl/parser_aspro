0e841d084c576d5e61fdbbd15184202e2079e6d1
skvorsovoleg <o.skvortsov@logema.org>
2025-02-17 16:52:13 +0300

    Поправил выводы, теперь выводится в csv с решением аспро

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,24 @@ import {stringify} from 'csv';
 
 (async () => {
   let time = performance.now();
+
   const browser = await puppeteer.launch({
     headless: false,
     args: ['--no-sandbox'],
   });
   const page = await browser.newPage();
+
+  await page.setRequestInterception(true);
+  await page.setJavaScriptEnabled(false);
+
+  page.on('request', req => {
+    if (req.resourceType() === 'stylesheet' || req.resourceType() === 'font' || req.resourceType() === 'image') {
+      req.abort();
+    } else {
+      req.continue();
+    }
+  })
+
   await page.goto(
     "https://aspro.ru/poweredbyaspro/",
     {waitUntil: "domcontentloaded"}
@@ -20,81 +33,76 @@ import {stringify} from 'csv';
 
   const urlSection: Array<string | null | any> = [];
 
-  const newPage = await browser.newPage();
-  await newPage.setRequestInterception(true);
-  await newPage.setJavaScriptEnabled(false);
-
-  newPage.on('request', req => {
-    if (req.resourceType() === 'stylesheet' || req.resourceType() === 'font' || req.resourceType() === 'image') {
-      req.abort();
-    } else {
-      req.continue();
-    }
-  })
-
   for (let link of sectionsLinks) {
     if (!link) {
       return null;
     }
-    await newPage.goto(link, {waitUntil: "domcontentloaded"});
-    const pageNumber: number = Number(await newPage.$eval('a.module-pagination__item:last-of-type', link => link.textContent));
+    await page.goto(link, {waitUntil: "domcontentloaded"});
+    const pageNumber: number = Number(await page.$eval('a.module-pagination__item:last-of-type', link => link.textContent));
 
     for (let i = 1; i < pageNumber; i++) {
       if (!(i === 1)) {
-        await newPage.goto(`${link}/?PAGEN_1=${i}`, {waitUntil: "domcontentloaded"});
+        await page.goto(`${link}/?PAGEN_1=${i}`, {waitUntil: "domcontentloaded"});
       }
 
-      const urls = await newPage.$$eval('a.catalog-list__item-site-link', links => {
-        return links.map(link => `https://${link.textContent?.trim()}`)
+      const sectionElement = await page.evaluate(() => {
+        const productInfo = document.querySelectorAll('.catalog-list__item-info');
+
+        return Array.from(productInfo).map((productInfoItem) => {
+          const productInfoUrl = productInfoItem.querySelector('a.catalog-list__item-site-link');
+          const productInfoSolution = productInfoItem.querySelector('span.catalog-list__item-prop-value > a');
+
+          return {
+            url: `https://${productInfoUrl?.textContent?.trim()}`,
+            solution: productInfoSolution?.textContent,
+          }
+        })
       })
-      urlSection.push(...urls);
+      urlSection.push(...sectionElement);
     }
   }
-  await newPage.close();
 
   const filename = "data.csv";
 
-  const columns = ['url', 'telephone', 'mail'];
+  const columns = ['URL-сайта', 'Решение аспро', 'Email', 'Телефон'];
   const writableStream = fs.createWriteStream(filename);
 
   const stringfier = stringify({header: true, columns});
 
-  for (let url of urlSection) {
-    const newPage = await browser.newPage();
+  for (let section of urlSection) {
     let telephone: string | null = "";
     let mail: string | null = "";
 
-    if (!url) {
-      await newPage.close();
+    if (!section.url) {
+      return null;
     }
 
-    const task = newPage.goto(url, {waitUntil: "domcontentloaded"});
+    const task = page.goto(section.url, {waitUntil: "domcontentloaded"});
     const wait = new Promise(r => setTimeout(r, 3000));
 
     try {
       await Promise.race([task, wait]);
     } catch (err) {
-      await newPage.close();
       continue;
     }
 
     try {
-      telephone = await newPage.$eval('a[href^="tel:"]', tel => tel.textContent);
+      telephone = await page.$eval('a[href^="tel:"]', tel => tel.textContent);
     } catch (error) {
       console.log(error)
     }
 
     try {
-      mail = await newPage.$eval('a[href^="mailto:"]', mail => mail.textContent);
+      mail = await page.$eval('a[href^="mailto:"]', mail => mail.textContent);
     } catch (error) {
       console.log(error);
     }
 
-    stringfier.write([url, telephone, mail]);
-    await newPage.close();
+    stringfier.write([section.url, section.solution, mail, telephone]);
   }
-
   stringfier.pipe(writableStream);
+
+  await page.close();
   await browser.close();
 })();
 
