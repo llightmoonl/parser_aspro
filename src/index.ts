1157dca2d78a405d6a50390819a6544d32c4bc10
skvorsovoleg <o.skvortsov@logema.org>
2025-02-17 15:23:25 +0300

    добавил работу с csv, убрал стили, шрифты, кнопки

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import puppeteer from 'puppeteer';
-import {generate, stringify} from 'csv';
-
+import fs from 'fs';
+import {stringify} from 'csv';
 
 (async () => {
+  let time = performance.now();
   const browser = await puppeteer.launch({
     headless: false,
     args: ['--no-sandbox'],
@@ -18,34 +19,57 @@ import {generate, stringify} from 'csv';
   })
 
   const urlSection: Array<string | null | any> = [];
-  const contactsUrls = [];
+
+  const newPage = await browser.newPage();
+  await newPage.setRequestInterception(true);
+  await newPage.setJavaScriptEnabled(false);
+
+  newPage.on('request', req => {
+    if (req.resourceType() === 'stylesheet' || req.resourceType() === 'font' || req.resourceType() === 'image') {
+      req.abort();
+    } else {
+      req.continue();
+    }
+  })
 
   for (let link of sectionsLinks) {
-    const newPage = await browser.newPage();
-
     if (!link) {
-      await newPage.close();
       return null;
     }
     await newPage.goto(link, {waitUntil: "domcontentloaded"});
+    const pageNumber: number = Number(await newPage.$eval('a.module-pagination__item:last-of-type', link => link.textContent));
 
-    const urls = await newPage.$$eval('a.catalog-list__item-site-link', links => {
-      return links.map(link => `https://${link.textContent}`)
-    })
-    urlSection.push(...urls);
+    for (let i = 1; i < pageNumber; i++) {
+      if (!(i === 1)) {
+        await newPage.goto(`${link}/?PAGEN_1=${i}`, {waitUntil: "domcontentloaded"});
+      }
 
-    await newPage.close();
+      const urls = await newPage.$$eval('a.catalog-list__item-site-link', links => {
+        return links.map(link => `https://${link.textContent?.trim()}`)
+      })
+      urlSection.push(...urls);
+    }
   }
+  await newPage.close();
+
+  const filename = "data.csv";
+
+  const columns = ['url', 'telephone', 'mail'];
+  const writableStream = fs.createWriteStream(filename);
+
+  const stringfier = stringify({header: true, columns});
 
   for (let url of urlSection) {
     const newPage = await browser.newPage();
+    let telephone: string | null = "";
+    let mail: string | null = "";
 
     if (!url) {
       await newPage.close();
     }
 
     const task = newPage.goto(url, {waitUntil: "domcontentloaded"});
-    const wait = new Promise(r => setTimeout(r, 10000));
+    const wait = new Promise(r => setTimeout(r, 3000));
 
     try {
       await Promise.race([task, wait]);
@@ -54,31 +78,23 @@ import {generate, stringify} from 'csv';
       continue;
     }
 
-    let telephone: string | null = "";
-    let mail: string | null = "";
-
     try {
       telephone = await newPage.$eval('a[href^="tel:"]', tel => tel.textContent);
-    } catch {
-      telephone = "";
+    } catch (error) {
+      console.log(error)
     }
 
     try {
       mail = await newPage.$eval('a[href^="mailto:"]', mail => mail.textContent);
-    } catch {
-      mail = "";
+    } catch (error) {
+      console.log(error);
     }
-    contactsUrls.push({url, telephone, mail});
+
+    stringfier.write([url, telephone, mail]);
     await newPage.close();
   }
 
-  const data = stringify(contactsUrls, {
-    header: true,
-    delimiter: ' '
-  })
-
-  console.log(data);
-
+  stringfier.pipe(writableStream);
   await browser.close();
 })();
 
